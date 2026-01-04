4df7a033413811803815ab65293735ccd6f79997
PDJ6975 <antrodcal@alum.us.es>
2026-01-04 17:06:36 +0100

    feat: modelo base en django

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Valoracion
 
-# Register your models here.
+
+@admin.register(Valoracion)
+class ValoracionAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'titulo_libro', 'puntuacion', 'fecha')
+    list_filter = ('puntuacion', 'fecha', 'usuario')
+    search_fields = ('titulo_libro', 'usuario__username')
+    readonly_fields = ('fecha', 'actualizado')
+    ordering = ('-fecha',)
+    
+    fieldsets = (
+        ('Información del libro', {
+            'fields': ('titulo_libro',)
+        }),
+        ('Valoración', {
+            'fields': ('usuario', 'puntuacion')
+        }),
+        ('Fechas', {
+            'fields': ('fecha', 'actualizado'),
+            'classes': ('collapse',)
+        }),
+    )
--- main/models.py
+++ main/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Valoracion(models.Model):
+    """
+    Valoración de un usuario sobre un libro.
+    El libro se identifica por su título (almacenado en Whoosh).
+    """
+    usuario = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='valoraciones',
+        verbose_name='Usuario'
+    )
+    titulo_libro = models.CharField(
+        max_length=500,
+        verbose_name='Título del libro'
+    )
+    puntuacion = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        verbose_name='Puntuación',
+        help_text='Valoración de 1 a 5 estrellas'
+    )
+    fecha = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Fecha de valoración'
+    )
+    actualizado = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Última actualización'
+    )
+    
+    class Meta:
+        verbose_name = 'Valoración'
+        verbose_name_plural = 'Valoraciones'
+        unique_together = ('usuario', 'titulo_libro')
+        ordering = ['-fecha']
+        indexes = [
+            models.Index(fields=['usuario', '-fecha']),
+            models.Index(fields=['titulo_libro']),
+        ]
+    
+    def __str__(self):
+        return f"{self.usuario.username} - {self.titulo_libro}: {self.puntuacion}"
