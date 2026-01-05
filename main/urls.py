1c0ddd69ce2dbf5e760e44aaa2b5b109ebf1601d
PDJ6975 <antrodcal@alum.us.es>
2026-01-05 18:47:13 +0100

    feat: configuración inicial frontend

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('galeria/', views.galeria, name='galeria'),
+    path('mi-libreria/', views.mi_libreria, name='mi_libreria'),
+    path('login/', views.login, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from main.whoosh_utils import obtener_todos_libros
+from main.whoosh_utils import obtener_generos
 
 
 def index(request):
-    """Vista principal que muestra todos los libros"""
-    libros = obtener_todos_libros()
-    return render(request, 'main/index.html', {'libros': libros})
+    """Vista principal - Descubrir"""
+    generos = obtener_generos()
+    return render(request, 'main/index.html', {'generos': generos})
+
+
+def galeria(request):
+    """Vista Galería"""
+    return render(request, 'main/galeria.html')
+
+
+def mi_libreria(request):
+    """Vista Mi Librería"""
+    return render(request, 'main/mi_libreria.html')
+
+
+def login(request):
+    """Vista Login"""
+    return render(request, 'main/login.html')
